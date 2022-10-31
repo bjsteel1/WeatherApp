692423c2bc5aaf1de0c34818b233efc92e1cfccd
ivykowalski <irkowal1@svsu.edu>
2022-10-31 15:38:16 -0400

    Turned hourly button to image of a clock that when clicked on brings the user to the HourlyActivity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -8,8 +8,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    //create necessary views for main activity - Ivy
     Button btnLocation;
-    Button btnHourly;
+    ImageView ivClock;
     ImageView ivSettings;
 
     @Override
@@ -17,18 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing views
         btnLocation = findViewById(R.id.btnLocation);
-        btnHourly = findViewById(R.id.btnHourly);
+        ivClock = findViewById(R.id.ivClock);
+        ivClock.setImageResource(R.drawable.clock);
         ivSettings = findViewById(R.id.ivSettings);
         ivSettings.setImageResource(R.drawable.settings);
 
-        // Adding some buttons to navigate through activities when testing //
+        //ivLocation onClickListener that brings the user to the LocationActivity when the
+        //magnifying glass image is clicked
         btnLocation.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, LocationActivity.class));
         });
-        btnHourly.setOnClickListener(view -> {
+
+        //ivClock onClickListener that brings the user to the HourlyActivity when the
+        //clock image is clicked
+        ivClock.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, HourlyActivity.class));
         });
+
+        //ivSettings onClickListener that brings the user to the SettingsActivity when the
+        //settings image is clicked
         ivSettings.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, Settings.class));
         });
