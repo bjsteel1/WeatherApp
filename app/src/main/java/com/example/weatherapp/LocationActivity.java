aaba97ab37648671495b61764e3cc62180896b8a
jon-maddocks <56932287+jon-maddocks@users.noreply.github.com>
2022-10-31 16:28:40 -0400

    API partially implemented
    API is functional. Needs a loop to add more data. -jm

--- app/src/main/java/com/example/weatherapp/LocationActivity.java
+++ app/src/main/java/com/example/weatherapp/LocationActivity.java
@@ -16,7 +16,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class LocationActivity extends AppCompatActivity {
     EditText etSearch;
@@ -24,7 +34,11 @@ public class LocationActivity extends AppCompatActivity {
     RecyclerView recLocations;
     String TAG = "MYTAG";
 
+
     ArrayList<Location> arlLocations;
+    ArrayList<String> arlDefaultLocations;
+    RequestQueue queue;
+    LocationAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +51,34 @@ public class LocationActivity extends AppCompatActivity {
         btnAdd = findViewById(R.id.btnAdd);
         recLocations = findViewById(R.id.recLocations);
 
-        for(int i =0; i < 20; i++){
-            arlLocations.add(new Location("Mayville " + i, 10,10,10,10));
-        }
-        LocationAdapter adapter = new LocationAdapter(arlLocations, this);
+        arlDefaultLocations = new ArrayList<>();
+        arlDefaultLocations.add("Detroit");
+        arlDefaultLocations.add("New-York-City");
+        arlDefaultLocations.add("Chicago");
+        arlDefaultLocations.add("Houston");
+        arlDefaultLocations.add("Phoenix");
+        arlDefaultLocations.add("Philadelphia");
+        arlDefaultLocations.add("San-Antonio");
+        arlDefaultLocations.add("San-Diego");
+        arlDefaultLocations.add("Dallas");
+        arlDefaultLocations.add("San-Jose");
+
+        arlDefaultLocations.add("Paris");
+        arlDefaultLocations.add("London");
+        arlDefaultLocations.add("Bangkok");
+        arlDefaultLocations.add("Hong-Kong");
+        arlDefaultLocations.add("Dubai");
+        arlDefaultLocations.add("Singapore");
+        arlDefaultLocations.add("Rome");
+        arlDefaultLocations.add("Tokyo");
+        arlDefaultLocations.add("Seoul");
+        arlDefaultLocations.add("Sydney");
+
+        queue = Volley.newRequestQueue(this);
+        fetchData();
+
+
+        adapter = new LocationAdapter(arlLocations, this);
         recLocations.setAdapter(adapter);
         recLocations.setLayoutManager(new LinearLayoutManager(this));
 
@@ -53,9 +91,43 @@ public class LocationActivity extends AppCompatActivity {
 
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-
+                arlLocations.remove(viewHolder.getLayoutPosition());
+                adapter.notifyDataSetChanged();
             }
         });
+        helper.attachToRecyclerView(recLocations);
+    }
+
+    public void fetchData(){
+     //   String url = "https://api.weatherapi.com/v1/forecast.json?key=0d2ee64c9feb4ccc9ff23426222810&q=London&days=7&aqi=no&alerts=no";
+        String URL_1 ="https://api.weatherapi.com/v1/forecast.json?key=0d2ee64c9feb4ccc9ff23426222810&q=";
+        String URL_City = "";
+        String URL_2 = "&days=7&aqi=no&alerts=no";
+
+        String url = URL_1 + arlDefaultLocations.get(0) + URL_2;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONObject jLocation = response.getJSONObject("location");
+                        String strName = jLocation.getString("name");
+                        String strCountry = jLocation.getString("country");
+
+                        JSONObject jTemperature = response.getJSONObject("current");
+                        double dblTemperature = jTemperature.getDouble("temp_f");
+
+                        Location l = new Location(strName + ", " + strCountry, 10, 10, dblTemperature, 10);
+                        arlLocations.add(l);
+
+                        adapter.notifyDataSetChanged();
+                    } catch(Exception e){
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+
+                });
+        queue.add(request);
+
     }
 
 
