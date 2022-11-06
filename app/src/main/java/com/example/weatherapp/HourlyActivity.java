3c627c736687e1aa1f34c338646176e608955269
Clayton Parker <70655862+Stealthyworm111@users.noreply.github.com>
2022-11-06 13:48:25 -0500

    Final Touches on hourly activity barring anything else I notice

--- app/src/main/java/com/example/weatherapp/HourlyActivity.java
+++ app/src/main/java/com/example/weatherapp/HourlyActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,6 +37,8 @@ import java.util.Date;
 import java.util.Locale;
 
 public class HourlyActivity extends AppCompatActivity {
+    //declaring my variables
+    ImageView ivhome;
     TextView txtLocation;
     TextView txtMainTemp;
     TextView txtCoord;
@@ -46,7 +49,7 @@ public class HourlyActivity extends AppCompatActivity {
     Float Temp;
     String Weather;
     HourlyAdapter adapter;
-    String city,tempScale,format;
+    String city,tempScale,format,defaultLocation;
     int temp;
     SharedPreferences sharedPreferences;
     SimpleDateFormat TwentyFormat = new SimpleDateFormat("HH:mm");
@@ -58,45 +61,60 @@ public class HourlyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //shared prefs for settings
         sharedPreferences = getSharedPreferences("SharedPrefs",MODE_PRIVATE);
+        //sets up the unit temp and time
         tempScale = sharedPreferences.getString("Unit","f");
         format = sharedPreferences.getString("Time","12");
         tempScale=tempScale.toLowerCase();
         setContentView(R.layout.activity_hourly);
         queue = Volley.newRequestQueue(this);
         forecasts = new ArrayList<>();
-        fetchData();
+        //setting up views
         txtLocation = findViewById(R.id.txtLocation);
         txtMainTemp = findViewById(R.id.txtMainTemp);
         txtCoord = findViewById(R.id.txtCoord);
         rycHourly = findViewById(R.id.rycHourly);
+        ImageView ivHome = findViewById(R.id.ivHome);
+        //shared pref for location
+        SharedPreferences sharedSetLocation = getSharedPreferences("SharedPrefDefault", MODE_PRIVATE);
+        defaultLocation = sharedSetLocation.getString("default_location","Saginaw, USA");
+        //calls api
+        fetchData(defaultLocation);
 
-
-
-
+        //goes back home
+        ivHome.setOnClickListener(view->{
+            startActivity(new Intent(HourlyActivity.this, MainActivity.class));
+        });
 
     }
-    public void fetchData(){
+    public void fetchData(String location){
+        //creates the adapter
         adapter = new HourlyAdapter(forecasts,this);
-        String location = "Saginaw,Mi";
-        String url = "https://api.weatherapi.com/v1/forecast.json?key=0d2ee64c9feb4ccc9ff23426222810&q=" + location + "&aqi=no&alerts=no";
+        //url
+        String url = getResources().getString(R.string.url_part1)+ location + "&aqi=no&alerts=no";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,url,null,
                 response->{
                     try {
+                        //getting all JSON objects and arrays
                         JSONObject jsonForecast = response.getJSONObject("forecast");
                         JSONArray jsonDay = jsonForecast.getJSONArray("forecastday");
                         JSONArray jsonHourly = jsonDay.getJSONObject(0).getJSONArray("hour");
                         JSONObject jLocation = response.getJSONObject("location");
                         JSONObject jCurrent = response.getJSONObject("current");
+
                         temp = jCurrent.getInt("temp_"+tempScale);
                         city = jLocation.getString("name") + ", " + jLocation.getString("region");
                         double lat = jLocation.getDouble("lat");
                         double lon = jLocation.getDouble("lon");
+                        //for each hour in the day create a new forcast
+
                         for(int i = 0;i<jsonHourly.length();i++){
                             JSONObject jCondition = jsonHourly.getJSONObject(i).getJSONObject("condition");
                             Weather = jCondition.getString("icon");
                             Temp = Float.valueOf(jsonHourly.getJSONObject(i).getString("temp_"+tempScale));
                             time = jsonHourly.getJSONObject(i).getString("time").substring(11);
+                            //this just sets up the time
                             if(format.equals("12")){
                                 try {
                                     Date date = TwentyFormat.parse(time);
@@ -116,6 +134,7 @@ public class HourlyActivity extends AppCompatActivity {
                             HourlyForecast forecast0 = new HourlyForecast(Weather,Temp,time);
                             forecasts.add(forecast0);
                         }
+                        //sets data for non recycler view and calls the adapter with data
                         txtLocation.setText(city);
                         txtCoord.setText(lat + "°N, " + lon + " °W");
                         txtMainTemp.setText(temp+"°");
@@ -138,7 +157,7 @@ public class HourlyActivity extends AppCompatActivity {
         queue.add(request);
     }
 }
-
+//my class for hourly forcase
 class HourlyForecast{
     String weather;
     Float temperature;
@@ -151,6 +170,7 @@ class HourlyForecast{
     }
 
 }
+//standard adapter code from class
     class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.HourlyViewHolder>{
     ArrayList<HourlyForecast> forecasts;
     Context context;
@@ -174,6 +194,7 @@ class HourlyForecast{
         holder.txtTemperture.setText(Math.round(forecast.temperature)+"°");
         holder.txtTime.setText(forecast.time+"");
 
+        //loads the image
         Picasso.get().load("https:"+forecast.weather).into(holder.ivWeatherType);
         Picasso.get().setLoggingEnabled(true);
     }
@@ -185,6 +206,7 @@ class HourlyForecast{
 
 
     class HourlyViewHolder extends RecyclerView.ViewHolder{
+        //sets up view holder
         ImageView ivWeatherType;
         TextView txtTime;
         TextView txtTemperture;
