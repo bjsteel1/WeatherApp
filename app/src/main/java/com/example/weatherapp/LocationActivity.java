a4655c7dda58a35a6f7162a2851cb479ab876e1a
jon-maddocks <56932287+jon-maddocks@users.noreply.github.com>
2022-11-06 14:06:46 -0500

    New API key generated. API loading check
    A new API key has been generated and will last another 2-weeks. Swapping activities when the API is not finished calling will prompt an dialog msg.

--- app/src/main/java/com/example/weatherapp/LocationActivity.java
+++ app/src/main/java/com/example/weatherapp/LocationActivity.java
@@ -117,13 +117,15 @@ public class LocationActivity extends AppCompatActivity {
         });
         //Set OnClick Listener for click event on back image, and send user back to Home page
         imgBack.setOnClickListener(view -> {
-            //Save set/default location into String
-            // Default location is the top item in the RV. Save into String
-            SharedPreferences sharedSetLocation = getSharedPreferences("SharedPrefDefault", MODE_PRIVATE);
-            SharedPreferences.Editor spSetEditor = sharedSetLocation.edit();
-            spSetEditor.putString("default_location", adapter.getItem(0));
-            spSetEditor.apply();
-            startActivity(new Intent(LocationActivity.this, MainActivity.class));
+            if(loadCheck()){
+                //Save set/default location into String
+                // Default location is the top item in the RV. Save into String
+                SharedPreferences sharedSetLocation = getSharedPreferences("SharedPrefDefault", MODE_PRIVATE);
+                SharedPreferences.Editor spSetEditor = sharedSetLocation.edit();
+                spSetEditor.putString("default_location", adapter.getItem(0));
+                spSetEditor.apply();
+                startActivity(new Intent(LocationActivity.this, MainActivity.class));
+            }
         });
         //Set OnClick listener for click event on reset image, and delete every item in the
         //  RecyclerView and populate the list with default locations
@@ -362,5 +364,24 @@ public class LocationActivity extends AppCompatActivity {
         // All locations save into JSON Array
         spEditor.putString("jLocations", jsonArray.toString());
         spEditor.apply();
+    }
+
+    // Check to ensure no API item is still loading. Stop user until all items have been loaded
+    public boolean loadCheck(){
+        for(int i = 0; i < arlLocations.size(); i++){
+            if(arlLocations.get(i).cityName.equalsIgnoreCase("Loading")){
+                AlertDialog.Builder errorBuilder = new AlertDialog.Builder(LocationActivity.this);
+                errorBuilder.setMessage("Please wait one moment. . .");
+                errorBuilder.setTitle("API ALERT!");
+                errorBuilder.setCancelable(false);
+                errorBuilder.setPositiveButton("OK", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    dialog.cancel();
+                });
+                AlertDialog ad = errorBuilder.create();
+                ad.show();
+                return false;
+            }
+        }
+        return true;
     }
 }
